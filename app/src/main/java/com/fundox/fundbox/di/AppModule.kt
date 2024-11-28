e8793a229763661e1d48650bece46b2d7cd9fdc3
shubhampandey45 <shubhampandey5410@gmail.com>
2024-11-28 19:27:29 +0530

    conflict resolve

--- app/src/main/java/com/fundox/fundbox/di/AppModule.kt
+++ app/src/main/java/com/fundox/fundbox/di/AppModule.kt
@@ -1,10 +1,119 @@
+@file:Suppress("DEPRECATION")
+
 package com.fundox.fundbox.di
 
+import android.app.Application
+import android.content.Context
+import com.fundox.fundbox.R
+import com.fundox.fundbox.core.Constants.SIGN_IN_REQUEST
+import com.fundox.fundbox.core.Constants.SIGN_UP_REQUEST
+import com.fundox.fundbox.data.repository.AuthRepositoryImpl
+import com.fundox.fundbox.data.repository.ProfileRepositoryImpl
+import com.fundox.fundbox.domain.repository.AuthRepository
+import com.fundox.fundbox.domain.repository.ProfileRepository
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Named
 
 @Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
+@InstallIn(ViewModelComponent::class)
+class AppModule {
+    @Provides
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    fun provideFirebaseFirestore() = Firebase.firestore
+
+    @Provides
+    fun provideOneTapClient(
+        @ApplicationContext
+        context: Context
+    ) = Identity.getSignInClient(context)
+
+    @Provides
+    @Named(SIGN_IN_REQUEST)
+    fun provideSignInRequest(
+        app: Application
+    ) = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        )
+        .setAutoSelectEnabled(true)
+        .build()
+
+    @Provides
+    @Named(SIGN_UP_REQUEST)
+    fun provideSignUpRequest(
+        app: Application
+    ) = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        )
+        .build()
+
+    @Provides
+    fun provideGoogleSignInOptions(
+        app: Application
+    ) = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(app.getString(R.string.web_client_id))
+        .requestEmail()
+        .build()
+
+    @Provides
+    fun provideGoogleSignInClient(
+        app: Application,
+        options: GoogleSignInOptions
+    ) = GoogleSignIn.getClient(app, options)
+
+    @Provides
+    fun provideAuthRepository(
+        auth: FirebaseAuth,
+        oneTapClient: SignInClient,
+        @Named(SIGN_IN_REQUEST)
+        signInRequest: BeginSignInRequest,
+        @Named(SIGN_UP_REQUEST)
+        signUpRequest: BeginSignInRequest,
+        db: FirebaseFirestore
+    ): AuthRepository = AuthRepositoryImpl(
+        auth = auth,
+        oneTapClient = oneTapClient,
+        signInRequest = signInRequest,
+        signUpRequest = signUpRequest,
+        db = db
+    )
+
+    @Provides
+    fun provideProfileRepository(
+        auth: FirebaseAuth,
+        oneTapClient: SignInClient,
+        signInClient: GoogleSignInClient,
+        db: FirebaseFirestore
+    ): ProfileRepository = ProfileRepositoryImpl(
+        auth = auth,
+        oneTapClient = oneTapClient,
+        signInClient = signInClient,
+        db = db
+    )
 }
